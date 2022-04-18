6ac27001433b8a78c6b9e2bef542e1256673cc2b
Brian Cruz Sanchez <BrianCS1999@outlook.com>
2022-04-18 11:29:29 -0500

    1.1.4

--- js/app.js
+++ js/app.js
@@ -134,19 +134,20 @@ particlesJS(
 
 const typed = new Typed(".typed", {
   strings: [
+    "[&nbsp;",
     "[&nbsp;Hi there!&nbsp;]",
     "[&nbsp;Welcome!&nbsp;]",
-    "[&nbsp;How are you?&nbsp;]",
-    "[&nbsp;Take care, goodbye!&nbsp;]",
+    // "[&nbsp;How are you?&nbsp;]",
+    // "[&nbsp;Take care, goodbye!&nbsp;]",
   ],
 
   typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
-  startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+  startDelay: 3, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
   backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
   smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
   shuffle: false, // Alterar el orden en el que escribe las palabras.
   backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
-  loop: true, // Repetir el array de strings
+  loop: false, // Repetir el array de strings
   loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
   showCursor: false, // Mostrar cursor palpitanto
   cursorChar: "|", // Caracter para el cursor
